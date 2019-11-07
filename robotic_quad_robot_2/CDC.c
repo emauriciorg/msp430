abe554eb7fe58becfcb223ddca92857fa74ffe82
s314e <emauriciorg@gmail.com>
2019-11-07 01:21:20 -0500

    refactor: rename projects robotica to quad robot and added src files (removing functions form headers)

--- robotic_quad_robot_2/CDC.c
+++ robotic_quad_robot_2/CDC.c
@@ -1,42 +1,9 @@
-/*
- * CDC._UD=(datoint&0x000f);h
- *
- *  Created on: 18/10/2014
- *      Author: mauricio
- *
- *      CDC is an acronym for Communication and Data Converter
- *      This library contains the functions necessary to handle in a easy way the
- *      transfer of data(char strings, decimal numer, hexadecimal, special character,etc.) in
- *      uart-mode, basically this library transform your int, unsigned int or  float in a
- *      string of char type variable, and send it for you.
- *
- *------------------------------FUNCTIONS-----------------------------
- *ESPACIO();
- *BORRAR();
- *
- *
- *
- *
- */
-#ifndef CDC_H_
-#define CDC_H_
-/*Function List
- *
- * borrar();
- * espacio();
- * c2ui();
- * ec();
- * enviardato();
- * int2hex();
- * printchar2c();
- * printfloar();
- * printlong();
- * printuint();
- * readint();
- * twoc2dec();
- * string2int();
- *
- * */
+#include "CDC.h"
+
+/*!
+   \todo refactor functions use efficient conversions and use english namin with
+   snake format
+*/
 char aux_char[7];
 char aux_char1[10];
 char soyunvector[];
@@ -226,5 +193,3 @@ void printfloat(float twoto3)
 	aux_char[5]=(char)(_U+48);
 	ec(aux_char);
 }
-
-#endif /* CDC_H_ */
---
+++ robotic_quad_robot_2/CDC.h
@@ -0,0 +1,41 @@
+/*
+ * CDC._UD=(datoint&0x000f);h
+ *
+ *  Created on: 18/10/2014
+ *      Author: mauricio
+ *
+ *      CDC is an acronym for Communication and Data Converter
+ *      This library contains the functions necessary to handle in a easy way the
+ *      transfer of data(char strings, decimal numer, hexadecimal, special character,etc.) in
+ *      uart-mode, basically this library transform your int, unsigned int or  float in a
+ *      string of char type variable, and send it for you.
+ *
+ *------------------------------FUNCTIONS-----------------------------
+ *ESPACIO();
+ *BORRAR();
+ *
+ *
+ *
+ *
+ */
+#ifndef CDC_H_
+#define CDC_H_
+/*Function List
+ *
+ * borrar();
+ * espacio();
+ * c2ui();
+ * ec();
+ * enviardato();
+ * int2hex();
+ * printchar2c();
+ * printfloar();
+ * printlong();
+ * printuint();
+ * readint();
+ * twoc2dec();
+ * string2int();
+ *
+ * */
+
+#endif /* CDC_H_ */
